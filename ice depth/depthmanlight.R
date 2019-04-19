6c7bf3488f9726a7b43aa3211efab0bc4c2402db
EmilyWhitaker <ewhitaker524@gmail.com>
2019-04-19 16:36:26 -0500

    new

--- ice depth/depthmanlight.R
+++ ice depth/depthmanlight.R
@@ -2,4 +2,57 @@
 
 #remove all the depths that arent zeros
 
-basedata %>% filter(depth == 0)
+all3 <-
+  all_2 %>% 
+  filter(depth == 0)
+
+all4 <- as.Date(all3, sampledate, c(%Y-%m-%d))
+  
+
+all3 <-
+  all3 %>%
+  separate(sampledate, c("month", "day", "year"), sep= "/", extra = "merge")
+
+all3 <-
+  all3 %>%
+  mutate(
+    month = as.numeric(month)
+  )
+
+all3 <-
+  all3 %>%
+  mutate(
+    year = as.numeric(year)
+  )
+
+a2 <-
+  basedatadates%>% 
+  filter(depth == 0)
+
+ggplot(data = all3, mapping = aes(x = month, y = light)) +
+  geom_point(aes(color= lakeid)) +
+  facet_wrap("lakeid")
+  
+
+ggplot(data = all3, mapping = aes(x = year, y = light)) +
+  geom_point(aes(color= lakeid)) +
+  facet_wrap("lakeid")
+
+
+
+#group into winter and summer 
+
+
+
+
+#find ice on and ice off lable as winter and summer 
+
+dtf = iceDatesCombo %>% dplyr::select(lakeid,iceon,iceoff) %>%
+  gather(group,date,iceon:iceoff) %>%
+  dplyr::filter(lakeid != 'LR') %>%
+  left_join(lakestats,by = c('lakeid'='LakeAbr'))
+
+basedatadates <-
+  basedata %>%
+  separate(sampledate, c("year", "month", "day"), sep= "-", extra = "merge")
+  
