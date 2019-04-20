63e311f7a99da72ec39d4d86d54b611bfd99bc74
EmilyWhitaker <ewhitaker524@gmail.com>
2019-04-20 08:32:30 -0500

    graphs

--- ice depth/depthmanlight.R
+++ ice depth/depthmanlight.R
@@ -6,28 +6,9 @@ all3 <-
   all_2 %>% 
   filter(depth == 0)
 
-all4 <- as.Date(all3, sampledate, c(%Y-%m-%d))
-  
 
-all3 <-
-  all3 %>%
-  separate(sampledate, c("month", "day", "year"), sep= "/", extra = "merge")
+all3$sampledate <- as.Date(all3$sampledate , "%y/%m/%d")
 
-all3 <-
-  all3 %>%
-  mutate(
-    month = as.numeric(month)
-  )
-
-all3 <-
-  all3 %>%
-  mutate(
-    year = as.numeric(year)
-  )
-
-a2 <-
-  basedatadates%>% 
-  filter(depth == 0)
 
 ggplot(data = all3, mapping = aes(x = month, y = light)) +
   geom_point(aes(color= lakeid)) +
@@ -37,6 +18,41 @@ ggplot(data = all3, mapping = aes(x = month, y = light)) +
 ggplot(data = all3, mapping = aes(x = year, y = light)) +
   geom_point(aes(color= lakeid)) +
   facet_wrap("lakeid")
+
+ggplot(all3, mapping = aes(x = sampledate, y = light))+
+  geom_point(aes(color= lakeid))
+
+
+lakestats = read_csv('LTERlakes.csv') 
+
+dml = iceDatesCombo %>% dplyr::select(lakeid,iceon,iceoff) %>%
+  gather(group,date,iceon:iceoff) %>%
+  dplyr::filter(lakeid != 'LR') %>%
+  left_join(all3,by = c('lakeid')) %>%
+  left_join(lakestats,by = c('lakeid'='LakeAbr'))
+
+
+
+ggplot(dml,aes(x = sampledate,  fill = group, y = light)) +
+  scale_x_date(sampledate_breaks = "1 month", sampledate_labels =  "%b") +
+  geom_density_ridges(scale = 2,alpha = .5,rel_min_height = 0.01,
+                      jittered_points = TRUE,
+                      position = position_points_jitter(width = 0.05, height = 0),
+                      point_shape = '|', point_size = 1) + 
+  scale_fill_discrete(name = '') + 
+  labs(y = '',x = '') 
+
+
+ggplot(dml,aes(x = sampledate,  fill = group, y = light)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b")  +
+  scale_y_discrete(expand = c(0.01, 0)) +
+  geom_density_ridges(scale = 2,alpha = .5,rel_min_height = 0.01,
+                      jittered_points = TRUE,
+                      position = position_points_jitter(width = 0.05, height = 0),
+                      point_shape = '|', point_size = 1) + 
+  scale_fill_discrete(name = '') + 
+  labs(y = '',x = '')
+
 
 
 
--- ice depth/unsure.R
+++ ice depth/unsure.R
@@ -2,15 +2,30 @@ library(lubridate)
 library(ggplot2)
 library(ggridges)
 library(tidyverse)
-basedata %>% filter(depth == 0)
+install.packages(ggjoy)
+library(ggjoy)
+#spilt indivually then join at depth 0? 
 
-iceDatesN = read_csv('ntl32_v5.csv')  # Northern
+
+#first manipulate for PAR @ 0
+Total_lights <- 
+  
+  
+  
+  
+# Calculate duration of northern ice
+iceDatesN = read_csv('ntl32_v5.csv') # Northern
+iceDatesS = read_csv('ntl33_v4.csv') %>% # Southern
+  mutate(firstice = as.Date(ice_on,'%m/%d/%Y'), lastice = as.Date(ice_off,'%m/%d/%Y'), ice_duration = as.numeric(ice_duration), year4 = lead(as.numeric(year4))) %>%
+  dplyr::select(year = year4,lakeid,ice_duration, firstice, lastice)
+
 
 iceDatesCombo = iceDatesN %>% 
   mutate(datefirsticeN = lag(datefirstice), firsticeN = lag(firstice)) %>%
   dplyr::mutate(firstIceX = ifelse(firsticeN < 100,0-firsticeN,365 - firsticeN), duration = lastice + firstIceX) %>%
   dplyr::mutate(firstIceX = ifelse(firsticeN < 100,0-firsticeN,365 - firsticeN), duration = lastice + firstIceX) %>%
   dplyr::select(year, lakeid, ice_duration = duration, firstice = datefirsticeN, lastice = datelastice) %>%
+  bind_rows(iceDatesS) %>%
   mutate(firsticeYDAY = ifelse(yday(firstice) <50, yday(firstice) + 365,yday(firstice)), lasticeYDAY = yday(lastice)) %>%
   mutate(iceon = as.Date('2000-01-01') + firsticeYDAY, iceoff = as.Date('2001-01-01') + lasticeYDAY)
 
@@ -21,10 +36,8 @@ dtf = iceDatesCombo %>% dplyr::select(lakeid,iceon,iceoff) %>%
   dplyr::filter(lakeid != 'LR') %>%
   left_join(lakestats,by = c('lakeid'='LakeAbr'))
 
-
-
-ggplot(dml,aes(x = sampledate,  fill = light,
-               y = reorder(Characteristic,Latitude,function(x)-min(-x)))) +
+ggplot(dtf,aes(x = date,  fill = group,
+               y = light)) +
   scale_x_date(date_breaks = "1 month", date_labels =  "%b")  +
   geom_density_ridges(scale = 2,alpha = .5,rel_min_height = 0.01,
                       jittered_points = TRUE,
@@ -34,16 +47,4 @@ ggplot(dml,aes(x = sampledate,  fill = light,
   labs(y = '',x = '') 
 
 
-
-  facet_grid("lakeid")
-
-ggplot(dml,aes(x = date,  fill = light, y = reorder(Characteristic,Latitude,function(x)-min(-x)))) +
-  scale_x_date(date_breaks = "1 month", date_labels =  "%b")  +
-  geom_density_ridges(scale = 2,alpha = .5,rel_min_height = 0.01,
-                      jittered_points = TRUE,
-                      position = position_points_jitter(width = 0.05, height = 0),
-                      point_shape = '|', point_size = 1) + 
-  scale_fill_discrete(name = '') + 
-  labs(y = '',x = '')
-  
-ggsave('icedates2.png', width = 7, height = 4)
+theme_joy() +
